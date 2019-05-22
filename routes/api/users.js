d31216fabc2927d18a95443997843c7cabf52d8a
Justin Parado <justin.parado@dice205.com>
2019-05-22 09:55:29 +0800

    Check validator

---
+++ routes/api/users.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+
+// @route		POST api/users
+// @desc		Register user
+// @access	Public
+router.post('/', [
+	check('name', 'Name is required')
+	.not()
+	.isEmpty(),
+	check('email', 'Please include a valid email').isEmail(),
+	check('password', 'Please enter a password with 6 or more characters'),
+], (req, res) => {
+	console.log(req.body)
+	res.send('User route')
+})
+
+module.exports = router;
