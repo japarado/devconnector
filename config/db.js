ccf3c9197a66f109c6668686d0514bce167bdfe4
Justin Parado <justin.parado@dice205.com>
2019-05-24 11:32:44 +0800

    Finished profile

--- config/db.js
+++ config/db.js
@@ -1,19 +1,25 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 const connectDB = async () => 
 {
-	try {
-		await mongoose.connect(db, { useNewUrlParser: true })
+	try 
+	{
+		await mongoose.connect(db, 
+			{ 
+				useNewUrlParser: true, 
+				useCreateIndex: true,
+				useFindAndModify: false,
+			});
 
-		console.log('MongoDB Connected...');
+		console.log("MongoDB Connected...");
 	}
 	catch(err)
 	{
-		console.error(err)
+		console.error(err);
 		process.exit(1);
 	}
-}
+};
 
 module.exports = connectDB;
